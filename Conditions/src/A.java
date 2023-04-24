6e1f9305823405a6321169c9a53111c9207042c3
Sineth <sinethshashintha2000@gmail.com>
2023-04-24 16:57:47 +0530

    conditions

---
+++ Conditions/src/A.java
@@ -0,0 +1,13 @@
+public class A {
+        public static void main(String[] args) {
+
+            int a = 10;
+            int b = 20;
+            if(a<b){
+                System.out.println("Big");
+            }
+            else {
+                System.out.println("Small");
+            }
+    }
+}
---
+++ Conditions/src/B.java
@@ -0,0 +1,15 @@
+public class B {
+
+    public static void main(String[] args) {
+
+        String a = "Sineth";
+        if (a == "Sineth") {
+            System.out.println("Sineth");
+        } else if (a == "Shashintha") {
+            System.out.println("ShashinthA");
+        }
+    else{
+            System.out.println("Wijethung");
+        }
+    }
+}
